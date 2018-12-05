kubernetes/kubernetes

IsMigratedToCSI() plugin interface change and migration logic for Persistent Volumes for Attach/Detach only

pkg/volume/csi/csi_attacher.go
@@ -400,20 +400,38 @@ var _ volume.Detacher = &csiAttacher{}
 var _ volume.DeviceUnmounter = &csiAttacher{}
 
 func (c *csiAttacher) Detach(volumeName string, nodeName types.NodeName) error {
-	// volumeName in format driverName<SEP>volumeHandle generated by plugin.GetVolumeName()
+	var attachID string
+	var volID string
+
 	if volumeName == "" {
 		glog.Error(log("detacher.Detach missing value for parameter volumeName"))
 		return errors.New("missing expected parameter volumeName")
 	}
-	parts := strings.Split(volumeName, volNameSep)
-	if len(parts) != 2 {
-		glog.Error(log("detacher.Detach insufficient info encoded in volumeName"))
-		return errors.New("volumeName missing expected data")
+
+	if isAttachmentName(volumeName) {
+		// Detach can also be called with the attach ID as the `volumeName`. This codepath is
+		// hit only when we have migrated an in-tree volume to CSI and the A/D Controller is shut
+		// down, the pod with the volume is deleted, and the A/D Controller starts back up in that
+		// order.
+		attachID = volumeName
+
+		// Vol ID should be the volume handle, except that is not available here.
+		// It is only used in log messages so in the event that this happens log messages will be
+		// printing out the attachID instead of the volume handle.
+		volID = volumeName
+	} else {
+		// volumeName in format driverName<SEP>volumeHandle generated by plugin.GetVolumeName()
+		parts := strings.Split(volumeName, volNameSep)
+		if len(parts) != 2 {
+			glog.Error(log("detacher.Detach insufficient info encoded in volumeName"))
+			return errors.New("volumeName missing expected data")
+		}
+
+		driverName := parts[0]
+		volID = parts[1]
+		attachID = getAttachmentName(volID, driverName, string(nodeName))
 	}
 
-	driverName := parts[0]
-	volID := parts[1]
-	attachID := getAttachmentName(volID, driverName, string(nodeName))
 	if err := c.k8s.StorageV1beta1().VolumeAttachments().Delete(attachID, nil); err != nil {
 		if apierrs.IsNotFound(err) {
 			// object deleted or never existed, done
@@ -579,10 +597,20 @@ func hasStageUnstageCapability(ctx context.Context, csi csiClient) (bool, error)
 	return stageUnstageSet, nil
 }
 
-// getAttachmentName returns csi-<sha252(volName,csiDriverName,NodeName>
+// getAttachmentName returns csi-<sha256(volName,csiDriverName,NodeName)>
 func getAttachmentName(volName, csiDriverName, nodeName string) string {
 	result := sha256.Sum256([]byte(fmt.Sprintf("%s%s%s", volName, csiDriverName, nodeName)))
 	return fmt.Sprintf("csi-%x", result)
+}
+
+// isAttachmentName returns true if the string given is of the form of an Attach ID
+// and false otherwise
+func isAttachmentName(unknownString string) bool {
+	// 68 == "csi-" + len(sha256hash)
+	if strings.HasPrefix(unknownString, "csi-") && len(unknownString) == 68 {
+		return true
+	}
+	return false
 }
 
 func makeDeviceMountPath(plugin *csiPlugin, spec *volume.Spec) (string, error) {

ddebroy/csi-migration-1

pkg/volume/csi/csi_attacher.go
@@ -405,20 +405,38 @@ var _ volume.Detacher = &csiAttacher{}
 var _ volume.DeviceUnmounter = &csiAttacher{}
 
 func (c *csiAttacher) Detach(volumeName string, nodeName types.NodeName) error {
-	// volumeName in format driverName<SEP>volumeHandle generated by plugin.GetVolumeName()
+	var attachID string
+	var volID string
+
 	if volumeName == "" {
 		klog.Error(log("detacher.Detach missing value for parameter volumeName"))
 		return errors.New("missing expected parameter volumeName")
 	}
-	parts := strings.Split(volumeName, volNameSep)
-	if len(parts) != 2 {
-		klog.Error(log("detacher.Detach insufficient info encoded in volumeName"))
-		return errors.New("volumeName missing expected data")
+
+	if isAttachmentName(volumeName) {
+		// Detach can also be called with the attach ID as the `volumeName`. This codepath is
+		// hit only when we have migrated an in-tree volume to CSI and the A/D Controller is shut
+		// down, the pod with the volume is deleted, and the A/D Controller starts back up in that
+		// order.
+		attachID = volumeName
+
+		// Vol ID should be the volume handle, except that is not available here.
+		// It is only used in log messages so in the event that this happens log messages will be
+		// printing out the attachID instead of the volume handle.
+		volID = volumeName
+	} else {
+		// volumeName in format driverName<SEP>volumeHandle generated by plugin.GetVolumeName()
+		parts := strings.Split(volumeName, volNameSep)
+		if len(parts) != 2 {
+			klog.Error(log("detacher.Detach insufficient info encoded in volumeName"))
+			return errors.New("volumeName missing expected data")
+		}
+
+		driverName := parts[0]
+		volID = parts[1]
+		attachID = getAttachmentName(volID, driverName, string(nodeName))
 	}
 
-	driverName := parts[0]
-	volID := parts[1]
-	attachID := getAttachmentName(volID, driverName, string(nodeName))
 	if err := c.k8s.StorageV1beta1().VolumeAttachments().Delete(attachID, nil); err != nil {
 		if apierrs.IsNotFound(err) {
 			// object deleted or never existed, done
@@ -570,10 +588,42 @@ func (c *csiAttacher) UnmountDevice(deviceMountPath string) error {
 	return nil
 }
 
+<<<<<<< HEAD
 // getAttachmentName returns csi-<sha252(volName,csiDriverName,NodeName>
+=======
+func hasStageUnstageCapability(ctx context.Context, csi csiClient) (bool, error) {
+	capabilities, err := csi.NodeGetCapabilities(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	stageUnstageSet := false
+	if capabilities == nil {
+		return false, nil
+	}
+	for _, capability := range capabilities {
+		if capability.GetRpc().GetType() == csipb.NodeServiceCapability_RPC_STAGE_UNSTAGE_VOLUME {
+			stageUnstageSet = true
+		}
+	}
+	return stageUnstageSet, nil
+}
+
+// getAttachmentName returns csi-<sha256(volName,csiDriverName,NodeName)>
+>>>>>>> IsMigratedToCSI() plugin interface change and migration logic for Persistent Volumes for Attach/Detach only
 func getAttachmentName(volName, csiDriverName, nodeName string) string {
 	result := sha256.Sum256([]byte(fmt.Sprintf("%s%s%s", volName, csiDriverName, nodeName)))
 	return fmt.Sprintf("csi-%x", result)
+}
+
+// isAttachmentName returns true if the string given is of the form of an Attach ID
+// and false otherwise
+func isAttachmentName(unknownString string) bool {
+	// 68 == "csi-" + len(sha256hash)
+	if strings.HasPrefix(unknownString, "csi-") && len(unknownString) == 68 {
+		return true
+	}
+	return false
 }
 
 func makeDeviceMountPath(plugin *csiPlugin, spec *volume.Spec) (string, error) {